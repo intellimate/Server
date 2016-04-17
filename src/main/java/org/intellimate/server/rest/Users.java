60c8f66c035d41862a3a443b0cb65b295204f0c9
LeanderK <Leander.Kurscheidt@gmx.de>
2016-04-17 22:08:10 +0200

    rest: finished Users

--- src/main/java/org/intellimate/server/rest/Users.java
+++ src/main/java/org/intellimate/server/rest/Users.java
@@ -5,9 +5,12 @@ import org.intellimate.server.BadRequestException;
 import org.intellimate.server.NotFoundException;
 import org.intellimate.server.UnauthorizedException;
 import org.intellimate.server.database.model.tables.records.UserRecord;
+import org.intellimate.server.database.operations.IzouInstanceOperations;
 import org.intellimate.server.database.operations.UserOperations;
+import org.intellimate.server.jwt.JWTHelper;
 import org.intellimate.server.jwt.JWTokenPassed;
 import org.intellimate.server.jwt.Subject;
+import org.intellimate.server.proto.IzouInstance;
 import org.intellimate.server.proto.User;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
@@ -20,9 +23,13 @@ import java.util.Objects;
  */
 public class Users {
     private final UserOperations userOperations;
+    private final IzouInstanceOperations izouInstanceOperations;
+    private final JWTHelper jwtHelper;
 
-    public Users(UserOperations userOperations) {
+    public Users(UserOperations userOperations, IzouInstanceOperations izouInstanceOperations, JWTHelper jwtHelper) {
         this.userOperations = userOperations;
+        this.izouInstanceOperations = izouInstanceOperations;
+        this.jwtHelper = jwtHelper;
     }
 
     /**
@@ -61,15 +68,52 @@ public class Users {
      * @param jwTokenPassed the jwt passed
      */
     public void removeUser(int id, JWTokenPassed jwTokenPassed) {
+        assertCorrectRequest(id, jwTokenPassed);
+        boolean existed = userOperations.deleteUser(id);
+        if (!existed) {
+            throw new NotFoundException(String.format("user %d already deleted", id));
+        }
+    }
+
+    /**
+     * adds a new instance for the user
+     * @param user the user to add the instance for
+     * @param name the name of the instance
+     */
+    public IzouInstance addIzouInstance(int user, String name, JWTokenPassed jwTokenPassed) {
+        assertCorrectRequest(user, jwTokenPassed);
+        int id = izouInstanceOperations.insertIzouInstance(user, name);
+        return IzouInstance.newBuilder()
+                .setId(id)
+                .setName(name)
+                .setToken(jwtHelper.generateIzouRefreshJWT(id))
+                .build();
+    }
+
+    /**
+     * removes the IzouInstance if the instance belongs to the user
+     * @param user the user associated
+     * @param instanceId the instance id
+     */
+    public void removeIzouInstance(int user, int instanceId, JWTokenPassed jwTokenPassed) {
+        assertCorrectRequest(user, jwTokenPassed);
+        boolean existed = izouInstanceOperations.removeIzouInstance(user, instanceId);
+        if (!existed) {
+            throw new NotFoundException(String.format("instance was not existing for user %d", user));
+        }
+    }
+
+    /**
+     * asserts that the passed id is the same as the jwt id and that the jwt type is user
+     * @param id the requested user id
+     * @param jwTokenPassed the jwt token
+     */
+    public void assertCorrectRequest(int id, JWTokenPassed jwTokenPassed) {
         if (jwTokenPassed.getSubject() != Subject.USER) {
             throw new BadRequestException("method only allowed for users");
         }
         if (id != jwTokenPassed.getId()) {
             throw new UnauthorizedException("not allowed to delete other user");
-        }
-        boolean existed = userOperations.deleteUser(id);
-        if (!existed) {
-            throw new NotFoundException(String.format("user %d already deleted", id));
         }
     }
 }
