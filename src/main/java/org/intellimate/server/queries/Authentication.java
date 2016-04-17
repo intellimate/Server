47fc650e21c589a8bf6d120e22ef0cd85b155f8a
LeanderK <Leander.Kurscheidt@gmx.de>
2016-04-17 19:49:19 +0200

    authentication: finished Authentication

--- src/main/java/org/intellimate/server/queries/Authentication.java
+++ src/main/java/org/intellimate/server/queries/Authentication.java
@@ -2,7 +2,7 @@ package org.intellimate.server.queries;
 
 import org.apache.commons.validator.routines.EmailValidator;
 import org.intellimate.server.BadRequestException;
-import org.intellimate.server.JWTHelper;
+import org.intellimate.server.jwt.JWTHelper;
 import org.intellimate.server.NotFoundException;
 import org.intellimate.server.UnauthorizedException;
 import org.intellimate.server.database.model.tables.records.UserRecord;
@@ -51,41 +51,15 @@ public class Authentication {
     }
 
     /**
-     * adds an user to the database
-     * @param username the chosen username, must be at least 4 characters long
-     * @param email the email
-     * @param password the chosen password, must be at least 4 characters long
-     * @throws BadRequestException if one of the parameters is invalid
-     */
-    public void addUser(String username, String email, String password) {
-        Objects.nonNull(username);
-        Objects.nonNull(email);
-        Objects.nonNull(password);
-        if (username.length() < 4) {
-            throw new BadRequestException("invalid username length, must be at least 4: " + username);
-        }
-        if (password.length() < 4) {
-            throw new BadRequestException("invalid password length, must be at least 4");
-        }
-        if (!EmailValidator.getInstance(false).isValid(email)) {
-            throw new BadRequestException("invalid email: " + email);
-        }
-        String hashpw = BCrypt.hashpw(password, BCrypt.gensalt());
-        UserRecord userRecord = new UserRecord(null, email, hashpw, username);
-        userOperations.insertUser(userRecord);
-    }
-
-    /**
      * handles the refresh requests from the izou-instances
      * @param id the id of the izou-instance from the refresh-token
      * @return if valid an access jwt-token
      */
-    public String refresh(String id) {
+    public String refresh(int id) {
         try {
-            int parsedID = Integer.parseInt(id);
-            boolean valid = izouInstanceOperations.validateIzouInstanceID(parsedID);
+            boolean valid = izouInstanceOperations.validateIzouInstanceID(id);
             if (valid) {
-                return jwtHelper.generateIzouAccessJWT(parsedID);
+                return jwtHelper.generateIzouAccessJWT(id);
             } else {
                 logger.info("invalid izou id", id);
                 throw new UnauthorizedException("Invalid JWT, the registered instance was removed");
