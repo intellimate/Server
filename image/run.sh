0552437bd68bff629ad91529348df873c9944654
LeanderK <Leander.Kurscheidt@gmx.de>
2016-05-14 00:18:35 +0200

    all:changed parameter names

--- image/run.sh
+++ image/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo The options are: $@
-if [[ -z "$SSL" && ! (-f keystore.jks) ]]; then
+if [[ -z "$SSL" && ! -f keystore.jks ]]; then
     echo generating keystore
     openssl pkcs12 -export -in /ssl/live/izou.info/fullchain.pem -inkey /ssl/live/izou.info/privkey.pem -out pkcs.p12 -name IZOU -passout pass: pass
     keytool -importkeystore -deststorepass IZOU -destkeypass IZOU -destkeystore keystore.jks -srckeystore pkcs.p12 -srcstoretype PKCS12 -srcstorepass pass -alias IZOU
