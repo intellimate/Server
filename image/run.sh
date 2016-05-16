ca044ef806a1e3aa7d78ba25ee0f5239ebc842b7
LeanderK <Leander.Kurscheidt@gmx.de>
2016-05-16 19:20:16 +0200

    run: new try with keytool

--- image/run.sh
+++ image/run.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 echo The options are: $@
-echo "SSL: $SSL"
-fileExists=$( -f keystore.jks )
-echo "file: $fileExists"
-if [[ "$SSL" = true ]]; then
-    echo ssl returned true
-fi
-if [[ ! -f keystore.jks ]]; then
-    echo file does not exist
-fi
 if [[ "$SSL" = true ]]; then
     echo ssl returned true
     if [[ ! -f keystore.jks ]]; then
         echo file does not exist
         echo generating keystore
         openssl pkcs12 -export -in /ssl/live/izou.info/fullchain.pem -inkey /ssl/live/izou.info/privkey.pem -out pkcs.p12 -name IZOU -passout pass:pass
-        keytool -importkeystore -deststorepass XL750BK -destkeystore keystore.jks -srckeystore pkcs.p12 -srcstoretype PKCS12 -srcstorepass pass -alias IZOU
+        keytool -importkeystore -deststorepass XL750BK -destkeypass XL750BK -destkeystore keystore.jks -srckeystore pkcs.p12 -srcstoretype PKCS12 -srcstorepass pass -alias IZOU
     fi
 fi
 java $@ -Drouter.port=4567 -jar bin/server.jar
