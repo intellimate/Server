872f8f3c8e0253e964e1f999bb049a7d552f04dc
LeanderK <Leander.Kurscheidt@gmx.de>
2016-05-16 18:34:34 +0200

    run: new try

--- image/run.sh
+++ image/run.sh
@@ -9,7 +9,7 @@ fi
 if [[ ! -f keystore.jks ]]; then
     echo file does not exist
 fi
-if [[ -z "$SSL" && ! -f keystore.jks ]]; then
+if [[ -z "$SSL" ]] && [[ ! -f keystore.jks ]]; then
     echo generating keystore
     openssl pkcs12 -export -in /ssl/live/izou.info/fullchain.pem -inkey /ssl/live/izou.info/privkey.pem -out pkcs.p12 -name IZOU -passout pass:pass
     keytool -importkeystore -deststorepass XL750BK -destkeystore keystore.jks -srckeystore pkcs.p12 -srcstoretype PKCS12 -srcstorepass pass -alias IZOU
