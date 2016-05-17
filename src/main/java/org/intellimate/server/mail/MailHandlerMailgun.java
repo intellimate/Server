6186127d93dc712298381b34f1b68b4409a7cec8
LeanderK <Leander.Kurscheidt@gmx.de>
2016-05-17 19:41:58 +0200

    mail: added mailgun

---
+++ src/main/java/org/intellimate/server/mail/MailHandlerMailgun.java
@@ -0,0 +1,37 @@
+package org.intellimate.server.mail;
+
+import net.sargue.mailgun.Configuration;
+import net.sargue.mailgun.MailBuilder;
+import org.intellimate.server.jwt.JWTHelper;
+
+import java.io.IOException;
+
+/**
+ * @author LeanderK
+ * @version 1.0
+ */
+public class MailHandlerMailgun extends MailHandler {
+    private final Configuration configuration;
+
+    public MailHandlerMailgun(String deliveryEmailAddress, JWTHelper jwtHelper, boolean disabled, String apikey) {
+        super(deliveryEmailAddress, jwtHelper, disabled);
+        if (!disabled) {
+            this.configuration = new Configuration()
+                    .domain("mg.izou.info")
+                    .apiKey(apikey)
+                    .from("Izou", deliveryEmailAddress);
+        } else {
+            this.configuration = null;
+        }
+    }
+
+    @Override
+    protected void sendMail(String receiver, String subject, String html) throws IOException {
+        MailBuilder.using(configuration)
+                .to(receiver)
+                .subject(subject)
+                .html(html)
+                .build()
+                .send();
+    }
+}
